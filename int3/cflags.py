aff178a065cf88ba4cfc31b572c7cc8b9f6ae027
Maurice Bailleu <M.Bailleu@web.de>
2018-08-23 00:16:32 +0100

    Moved filenames and path names generation to path.py

--- int3/cflags.py
+++ int3/cflags.py
@@ -3,25 +3,15 @@ import os
 from pathlib import Path
 
 from .errors import Int3Error
+from .path import find_int3_directory
 
 INT3_MACRO = "-DINT3=<int3/__FILE__-__LINE__>"
 
 
-def find_int3_directory() -> Path:
-    p = Path(os.getcwd())
-    while True:
-        int3_dir = p.joinpath(".int3")
-        if int3_dir.exists():
-            return int3_dir.resolve()
-        if p.parent == p:
-            raise Int3Error(".int3 directory not found")
-        p = p.parent
-
-
 def cflags_command(args: argparse.Namespace) -> None:
-    int3_dir = find_int3_directory()
-    inc_dir = int3_dir.joinpath("includes")
-    lib_dir = int3_dir.joinpath("lib")
+    int3_dirs = get_int3_dirs()
+    inc_dir = int3_dirs["includes"]
+    lib_dir = int3_dirs["lib"]
     t =f"""
 -I {inc_dir} {INT3_MACRO} -Wl,-rpath,{lib_dir} -L{lib_dir} -lint3
 """
--- int3/path.py
+++ int3/path.py
@@ -1,3 +1,30 @@
+import os
+
 from pathlib import Path
+from typing import Dict
+
+from .errors import Int3Error
 
 ROOT = Path(__file__).parent.parent.resolve()
+INT3_DIR = ".int3"
+
+def find_int3_directory() -> Path:
+    p = Path(os.getcwd())
+    while True:
+        int3_dir = p.joinpath(INT3_DIR)
+        if int3_dir.exists():
+            return int3_dir.resolve()
+        if p.parent == p:
+            raise Int3Error(".int3 directory not found")
+        p = p.parent
+
+def get_int3_dirs() -> Dict[str, Path]:
+    int3_dir = find_int3_directory()
+    return {
+            "includes" : int3_dir.joinpath("includes"),
+            "lib"      : int3_dir.joinpath("lib")
+            }
+
+def get_lib_src() -> Path:
+    lib_dir = get_int3_dirs()["lib"]
+    return lib_dir.joinpath("int3.cpp")
